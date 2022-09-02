747580ef3553b2d4185664d3dc37df00b017c055
Nikhil Swaminathan <nsswamin@amazon.com>
2022-09-02 13:52:38 -0700

    comit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import {Hero, NavBar} from './ui-components'
+import {Hero, NavBar} from '../ui-components'
 
 export default function Home() {
   return (
